73bc53c5afab14e0c882540a5df4e9456d3b9101
Wanzaz <ondra.pazourek@gmail.com>
2022-09-14 14:54:37 +0200

    Add theory of files

---
+++ lesson/files/theory.c
@@ -0,0 +1,17 @@
+/* 
+ * THEORY OF FILES
+
+The standard of input and output:
+	-stdin = input (keyboard)
+	-stdout = output (display)
+
+Files: 
+	- binary (opening with rb, wb modes)
+    - text
+
+Working with files 4. steps:
+	1. open file - FILE* (fopen(name_of_file, mode))
+	2. check if file is NULL (empty pointer)
+	3. check if EOF (End Of File) 
+	4. close file - fclose(name_of_file)
+*/
