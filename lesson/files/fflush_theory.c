f2ef82009e456b78351b9cd000593ab1ffc0a05a
Wanzaz <ondra.pazourek@gmail.com>
2022-09-21 16:28:42 +0200

    Add fflush() theory

---
+++ lesson/files/fflush_theory.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int num;
+	scanf("%d", &num);
+	/* fflush(stdin); */
+	char character;
+	scanf("%c", &character);
+
+
+	/* stdin and stdout are streams of data */
+
+	/* fprintf(stdout, "Hello"); = printf("Hello"); */
+
+	return 0;
+}
+
+
