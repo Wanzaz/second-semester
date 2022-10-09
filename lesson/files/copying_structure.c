4dc835d5c7ec522ddcc8c3a2886b5727aec04892
Wanzaz <ondra.pazourek@gmail.com>
2022-10-09 21:35:27 +0200

    Add function for listing saved data from the file to struct

--- lesson/files/copying_structure.c
+++ lesson/files/copying_structure.c
@@ -20,41 +20,31 @@ int countStudents(FILE *input, Tstudent a[]) {
 	return i;
 }
 
-/* int loadStructureToArray(FILE *input, Tstudent a[]) { */
-/* 	int i=0; */
-	
-/* 	while (i < MAX && fscanf(input, "%20s%d%d%d%d", a[i].name, &a[i].m, &a[i].p, &a[i].ch, &a[i].pr) == 5) { */
-/* 		i++; */
-/* 		a[i] = strcpy(a[i], a[i].name); */
-/* 	} */
-
-/* 	return i; */
-/* } */
-
-
-/* void listArray(int array) { */
-	
-
-/* } */
+void listStructure(Tstudent array[], int size) {
+	for (int i=0; i < size; i++) {
+		printf("%s m: %d, p: %d, ch: %d, pr: %d\n", array[i].name, array[i].m, array[i].p, array[i].ch, array[i].pr);
+	}
+}
 
 int main(int argc, char **argv) {
 
 	if (argc != 2) {
-		fprintf(stderr, "Run like this: program option inputfile outputfile\n");
+		fprintf(stderr, "Run like this: gcc students.c; ./a.out inputfile\n");
 		return EXIT_FAILURE;
 	}
 
 	Tstudent array[MAX];
 	char *inputpath = argv[1];
 
-	FILE* file = fopen(argv[1], "r");
+	FILE* file = fopen(inputpath, "r");
 	if (file == NULL) {
 		fprintf(stderr, "Input file %s is unable to open!\n", inputpath);
 		return EXIT_FAILURE;
 	}
 
-	int result = countStudents(file, array);
-	printf("Number of students: %d\n", result);
+	int size = countStudents(file, array);
+	printf("Number of students: %d\n", size);
+	listStructure(array, size);
 
 
 	fclose(file);
