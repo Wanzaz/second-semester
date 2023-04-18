77abf783263c91277d261fb82a860b476860d1c1
Wanzaz <ondra.pazourek@gmail.com>
2023-04-18 10:49:33 +0200

    Add joining tables in sql

---
+++ sql/03/join-table/db.sql
@@ -0,0 +1,44 @@
+CREATE TABLE persons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER);
+    
+INSERT INTO persons (name, age) VALUES ("Bobby McBobbyFace", 12);
+INSERT INTO persons (name, age) VALUES ("Lucy BoBucie", 25);
+INSERT INTO persons (name, age) VALUES ("Banana FoFanna", 14);
+INSERT INTO persons (name, age) VALUES ("Shish Kabob", 20);
+INSERT INTO persons (name, age) VALUES ("Fluffy Sparkles", 8);
+
+CREATE table hobbies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    person_id INTEGER,
+    name TEXT);
+    
+INSERT INTO hobbies (person_id, name) VALUES (1, "drawing");
+INSERT INTO hobbies (person_id, name) VALUES (1, "coding");
+INSERT INTO hobbies (person_id, name) VALUES (2, "dancing");
+INSERT INTO hobbies (person_id, name) VALUES (2, "coding");
+INSERT INTO hobbies (person_id, name) VALUES (3, "skating");
+INSERT INTO hobbies (person_id, name) VALUES (3, "rowing");
+INSERT INTO hobbies (person_id, name) VALUES (3, "drawing");
+INSERT INTO hobbies (person_id, name) VALUES (4, "coding");
+INSERT INTO hobbies (person_id, name) VALUES (4, "dilly-dallying");
+INSERT INTO hobbies (person_id, name) VALUES (4, "meowing");
+
+INSERT INTO persons (name, age) VALUES
+    ("Jack", 9)
+;
+INSERT INTO hobbies (person_id, name) VALUES
+    (5, "coding")
+;
+
+SELECT persons.name, hobbies.name FROM persons 
+    JOIN hobbies
+    ON persons.id = person_id
+    ;
+
+SELECT persons.name, hobbies.name FROM persons
+    JOIN hobbies
+    ON persons.id = person_id
+    WHERE persons.name == "Bobby McBobbyFace"
+    ;
